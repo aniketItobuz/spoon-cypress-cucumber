b779ee44484adfbcc4269ffa98dddcba71230241
Aniket <aniket@itobuz.com>
2024-07-19 11:20:36 +0530

    code refactor

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,10 @@ const { defineConfig } = require("cypress");
 const cucumber = require('cypress-cucumber-preprocessor').default
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
   e2e: {
     setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on);
       // implement node event listeners here
       on('file:preprocessor', cucumber())
     },
