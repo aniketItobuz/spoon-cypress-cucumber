4e034c0a48794905d258df2e700594c39671f19b
Aniket <aniket@itobuz.com>
2024-07-19 13:23:33 +0530

    code refactor

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 const cucumber = require('cypress-cucumber-preprocessor').default
 
 module.exports = defineConfig({
+  projectId: 'aepdwb',
   reporter: 'cypress-mochawesome-reporter',
   e2e: {
     setupNodeEvents(on, config) {
